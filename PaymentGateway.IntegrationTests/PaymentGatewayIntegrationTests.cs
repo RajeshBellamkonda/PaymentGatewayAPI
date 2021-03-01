123cc3eaa87078a5e4fddb6ba12135ec798239d8
RajeshBellamkonda <rajeshbala.b@gmail.com>
2021-03-01 13:31:21 +0000

    Added more integration Tests

--- PaymentGateway.IntegrationTests/PaymentGatewayIntegrationTests.cs
+++ PaymentGateway.IntegrationTests/PaymentGatewayIntegrationTests.cs
@@ -2,8 +2,11 @@ using Newtonsoft.Json;
 using NUnit.Framework;
 using PaymentGateway.API.ViewModels;
 using PaymentGateway.IntegrationTests.Mocks;
+using PaymentGateway.Models;
 using System;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PaymentGateway.IntegrationTests
@@ -47,6 +50,64 @@ namespace PaymentGateway.IntegrationTests
             var errorMessage = await paymentResponse.Content.ReadAsStringAsync();
             Assert.IsNotNull(errorMessage);
             Assert.IsTrue(errorMessage.Equals("Requested payment details with Id InvalidId is not found", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        [Test]
+        public async Task Post_Payment_With_ValidDetails_Returns_Success()
+        {
+            // Arrange
+            var paymentRequest = GetPaymentRequest();
+            // Act
+            var paymentResponse = await Client.PostAsync("payment", new StringContent(JsonConvert.SerializeObject(paymentRequest), Encoding.UTF8, "application/json"));
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, paymentResponse.StatusCode);
+            Assert.IsNotNull(paymentResponse);
+
+            var paymentSummary = JsonConvert.DeserializeObject<PaymentSummaryVm>(await paymentResponse.Content.ReadAsStringAsync());
+            Assert.IsNotNull(paymentSummary);
+            Assert.IsNotNull(paymentSummary.Id);
+            Assert.IsNotNull(paymentSummary.Status);
+        }
+
+        [Test]
+        public async Task Post_Payment_With_InValidDetails_Returns_BadRequest()
+        {
+            // Arrange
+            var paymentRequest = GetPaymentRequest();
+            paymentRequest.CardNumber = null;
+
+            // Act
+            var paymentResponse = await Client.PostAsync("payment", new StringContent(JsonConvert.SerializeObject(paymentRequest), Encoding.UTF8, "application/json"));
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, paymentResponse.StatusCode);
+            Assert.IsNotNull(paymentResponse);
+
+            var errorMessage = await paymentResponse.Content.ReadAsStringAsync();
+            Assert.IsNotNull(errorMessage);
+            Assert.IsTrue(errorMessage.Contains("The CardNumber field is required.", StringComparison.InvariantCultureIgnoreCase));
+
+        }
+
+        private PaymentRequestVm GetPaymentRequest()
+        {
+            return new PaymentRequestVm
+            {
+                CardNumber = "1234-5678-9012-3456",
+                CardHolderName = "John Smith",
+                CardType = CardType.Master,
+                Cvv = 123,
+                Status = "Success",
+                Amount = 123,
+                ExpiryMonth = 9,
+                ExpiryYear = 2021,
+                Currency = "GBP",
+                Address1 = "Line 1",
+                City = "London",
+                Country = "UK",
+                PostCode = "EX17 1KB"
+            };
         }
     }
 }
